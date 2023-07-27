c897bd8789134916d0362975e1a41fcbfbff59d3
KeyboarT <mehmetszr05@gmail.com>
2023-07-27 08:37:22 +0300

    Backend side finished.

---
+++ index.js
@@ -0,0 +1,62 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY = "e5ZDqiwpMbIsx9dg57p39YzSrpeY1dmZKu1npuY_090";
+//Puregpt API_KEY = purgpt-6rwisbt7ta6pzl4zvwmpe
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+    //Fake API's url
+    //If this does not work one day...
+    //You can use following API's
+    //Puregpt = https://purgpt.xyz/v1/chat/completions
+    
+    const API_URL = "https://chimeragpt.adventblocks.cc/api/v1/chat/completions";
+    const messageElement = incomingChatLi.querySelector("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
+
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        messageElement.textContent = "Oops! Bi şeyler ters gitti. Lütfen tekrar deneyin.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if (!userMessage) return;
+    chatInput.value = "";
+    //Appends the user's message in the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Düşünüyor...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
